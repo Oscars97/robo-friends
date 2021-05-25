92c3df5d8f4e99705e8186f5ab46fec1788e0350
OscarSanchezM97 <oscarsanchezmeza97@gmail.com>
2021-05-25 17:41:23 -0600

    getting the users from jsonplaceholder api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import CardList from "./CardList";
-import { robots } from "./robots";
 import SearchBox from "./SearchBox";
-
+import "./App.css";
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
   }
@@ -15,17 +14,31 @@ class App extends React.Component {
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value });
   };
-
+  componentDidMount() {
+      setTimeout(()=>{
+          fetch('https://jsonplaceholder.typicode.com/users')
+      .then(data=>data.json())
+      .then(data=>{
+          this.setState({robots:data})
+      })
+      },3000)
+  };   
   render() {
     const filteredRobots = this.state.robots.filter((robot) => {
-      return robot.name
-        .toLowerCase()
-        .includes(this.state.searchfield.toLowerCase());
+      return (
+        robot.name
+          .toLowerCase()
+          .includes(this.state.searchfield.toLowerCase()) 
+          ||
+        robot.username
+          .toLowerCase()
+          .includes(this.state.searchfield.toLowerCase())
+      );
     });
-    
-    return (
+    return this.state.robots.length===0? <h1 className="big-title centered tc t1">Loading the robofriends...</h1>:
+     (
       <div className="tc">
-        <h1>ROBO FRIENDS</h1>
+        <h1 className="big-title f1">ROBOFRIENDS</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
